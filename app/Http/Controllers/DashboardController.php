f431ea4fb9c7686ff645c7e13c2f23493b344664
hatem <71508255+hatemfaysil16@users.noreply.github.com>
2022-10-11 14:38:38 +0200

    ss

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index(){
-        // return \view('dashboard.dashboard');
-        return \view('example');
+        return \view('dashboard.dashboard');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,4 @@
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-
-
-
-
 Route::get('/',[DashboardController::class,'index'])->name('/');
