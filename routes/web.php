6970ae4fc44b56dec7de2a4fc2d5efcc9a53b35b
Ahmed Mikkawe <ah.mikkawe@gmail.com>
2022-10-12 21:42:44 +0200

    created client, invoice, product, purchase controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
+
+use App\Http\Controllers\ClientController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\PurchaseController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -8,9 +13,10 @@ Route::group(['middleware' => ['auth']], function () {
     Route::get('/',[DashboardController::class,'index'])->name('/');
     Route::resource('users',UserController::class);
     Route::resource('roles',RoleController::class);
-
-
-
+    Route::resource("products", ProductController::class);
+    Route::resource("clients", ClientController::class);
+    Route::resource("purchases", PurchaseController::class);
+    Route::resource("invoices",InvoiceController::class);
 
 
 
