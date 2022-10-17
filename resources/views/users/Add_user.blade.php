341034d4db4d37ce9d90188aca5848d63159ebaa
Ahmed Mikkawe <ah.mikkawe@gmail.com>
2022-10-17 17:11:17 +0200

    changed add user view

--- resources/views/users/Add_user.blade.php
+++ resources/views/users/Add_user.blade.php
@@ -2,8 +2,10 @@
 @section('css')
 <!-- Internal Nice-select css  -->
 <link href="{{URL::asset('assets/plugins/jquery-nice-select/css/nice-select.css')}}" rel="stylesheet" />
+<link href="{{URL::asset('assets/plugins/fileuploads/css/fileupload.css')}}" rel="stylesheet" type="text/css"/>
+
 @section('title')
-اضافة مستخدم - مورا سوفت للادارة القانونية
+Add User
 @stop
 
 
@@ -13,8 +15,7 @@
 <div class="breadcrumb-header justify-content-between">
     <div class="my-auto">
         <div class="d-flex">
-            <h4 class="content-title mb-0 my-auto">المستخدمين</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ اضافة
-                مستخدم</span>
+            <h4 class="content-title mb-0 my-auto">Users</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ New User</span>
         </div>
     </div>
 </div>
@@ -32,7 +33,7 @@
             <button aria-label="Close" class="close" data-dismiss="alert" type="button">
                 <span aria-hidden="true">&times;</span>
             </button>
-            <strong>خطا</strong>
+            <strong>Error</strong>
             <ul>
                 @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
@@ -42,68 +43,59 @@
         @endif
 
         <div class="card">
-            <div class="card-body">
-                <div class="col-lg-12 margin-tb">
-                    <div class="pull-right">
-                        <a class="btn btn-primary btn-sm" href="{{ route('users.index') }}">رجوع</a>
-                    </div>
-                </div><br>
-                <form class="parsley-style-1" id="selectForm2" autocomplete="off" name="selectForm2" action="{{route('users.store','test')}}" method="post" enctype="multipart/form-data">
+            <div class="card-header">
+                <h4 class="card-title mb-1">User Information</h4>
+            </div>
+
+            <div class="card-body pt-0">
+                <form class="parsley-style-1 form-horizontal" id="selectForm2" autocomplete="off" name="selectForm2" action="{{route('users.store','test')}}" method="post" enctype="multipart/form-data">
                     {{csrf_field()}}
-
-                    <div class="">
-
-                        <div class="row mg-b-20">
-                            <div class="parsley-input col-md-6" id="fnWrapper">
-                                <label>اسم المستخدم: <span class="tx-danger">*</span></label>
-                                <input class="form-control form-control-sm mg-b-20"
-                                    data-parsley-class-handler="#lnWrapper" name="name" required="" type="text">
-                            </div>
-
-                            <div class="parsley-input col-md-6 mg-t-20 mg-md-t-0" id="lnWrapper">
-                                <label>البريد الالكتروني: <span class="tx-danger">*</span></label>
-                                <input class="form-control form-control-sm mg-b-20"
-                                    data-parsley-class-handler="#lnWrapper" name="email" required="" type="email">
-                            </div>
-                        </div>
-
-                    </div>
-
-                    <div class="row mg-b-20">
-                        <div class="parsley-input col-md-6 mg-t-20 mg-md-t-0" id="lnWrapper">
-                            <label>كلمة المرور: <span class="tx-danger">*</span></label>
-                            <input class="form-control form-control-sm mg-b-20" data-parsley-class-handler="#lnWrapper"
-                                name="password" required="" type="password">
-                        </div>
-
-                        <div class="parsley-input col-md-6 mg-t-20 mg-md-t-0" id="lnWrapper">
-                            <label> تاكيد كلمة المرور: <span class="tx-danger">*</span></label>
-                            <input class="form-control form-control-sm mg-b-20" data-parsley-class-handler="#lnWrapper"
-                                name="confirm-password" required="" type="password">
-                        </div>
-                    </div>
-
-                    <div class="row row-sm mg-b-20">
-                        <div class="col-lg-6">
-                            <label class="form-label">حالة المستخدم</label>
-                            <select name="Status" id="select-beast" class="form-control  nice-select  custom-select">
-                                <option value="مفعل">مفعل</option>
-                                <option value="غير مفعل">غير مفعل</option>
-                            </select>
-                        </div>
-                    </div>
-
-                    <div class="row mg-b-20">
-                        <div class="col-xs-12 col-md-12">
+                    <div class="row">
+                        <div class="col-sm-12 col-md-6">
                             <div class="form-group">
-                                <label class="form-label"> صلاحية المستخدم</label>
-                                {!! Form::select('roles_name[]', $roles,[], array('class' => 'form-control','multiple')) !!}
+                                <input type="text" class="form-control" id="quantity" name="name" placeholder="User Name" required>
                             </div>
                         </div>
+                        <div class="col-sm-12 col-md-6">
+                            <div class="form-group">
+                                <input class="form-control" type="email" name="email" placeholder="E-mail" required>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-md-6">
+                            <div class="form-group">
+                                <input class="form-control" type="password" name="password" placeholder="Password" required>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-md-6">
+                            <div class="form-group">
+                                <input class="form-control" type="password" name="confirm-password" placeholder="Confirm Password" required>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-md-6">
+                            <div class="form-group">
+                                <select class="form-control" name="Status" id="user-status">
+                                    <option label="Status">Status</option>
+                                    <option value="active">Active</option>
+                                    <option value="disabled">Disabled</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-md-6">
+                            <div class="form-group">
+                                {!! Form::select('roles_name[]', $roles,[], array('class' => 'form-control')) !!}
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-md-6">
+                            <div class="form-group">
+                                <input type="file" name="image" class="dropify" data-height="300" accept=".jpg, .png, image/jpeg, image/png" />
+                            </div>                        
+                        </div>
                     </div>
-                    @include('layouts.component.image.create')
-                    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                        <button class="btn btn-main-primary pd-x-20" type="submit">تاكيد</button>
+                    <div class="form-group mb-0 mt-3 justify-content-end">
+                        <div>
+                            <button type="submit" class="btn btn-primary">Add</button>
+                            <button type="reset" class="btn btn-secondary">Cancel</button>
+                        </div>
                     </div>
                 </form>
             </div>
@@ -122,6 +114,8 @@
 <!-- Internal Nice-select js-->
 <script src="{{URL::asset('assets/plugins/jquery-nice-select/js/jquery.nice-select.js')}}"></script>
 <script src="{{URL::asset('assets/plugins/jquery-nice-select/js/nice-select.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/fileuploads/js/fileupload.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/fileuploads/js/file-upload.js')}}"></script>                                
 
 <!--Internal  Parsley.min js -->
 <script src="{{URL::asset('assets/plugins/parsleyjs/parsley.min.js')}}"></script>
