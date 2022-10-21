a3deb31e43bccba096a16481564c62e6a84cfa4d
hatemfaysil16 <hatemfaysil16@gmail.com>
2022-10-21 17:16:36 +0200

    s

--- app/Actions/Typesofweight/UpdateTypesofweightAction.php
+++ app/Actions/Typesofweight/UpdateTypesofweightAction.php
@@ -4,7 +4,7 @@ namespace App\Actions\Typesofweight;
 use App\Models\Typesofweight;
 class UpdateTypesofweightAction
 {
-    public function handle(array $data,int $id):Typesofweight
+    public function handle(array $data,int $id):bool
     {
         return  Typesofweight::find($id)->update($data);
     }
